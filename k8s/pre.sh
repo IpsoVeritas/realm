be066d4f7e53265da54ef9e6d7675b74016cca1c
Lars Larsson <github@lars.dev>
2021-10-06 21:08:07 +0200

    remove old stuff

--- k8s/pre.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-vaultenv download secret/env/gitlab-ci/mailgun.yml mailgun.yml
-kubectl -n ${NAMESPACE} get secret mailgun || kubectl -n ${NAMESPACE} create secret generic mailgun --from-file=mailgun.yml
